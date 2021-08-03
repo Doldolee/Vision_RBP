fad044b94a895e553252388b13f9341a2cf48aab
임유석 <seook6853@naver.com>
2021-08-03 23:00:07 +0900

    faceRecognition

--- coral/detect.py
+++
@@ -1,3 +0,0 @@
-import collections
-import numpy as np
-Object = collections.namedtuple('Object', ['id', 'score', 'bbox']) class BBox(collections.namedtuple('BBox', ['xmin', 'ymin', 'xmax', 'ymax'])): """Bounding box. Represents a rectangle which sides are either vertical or horizontal, parallel to the x or y axis. """ __slots__ = () @ property def width(self):
